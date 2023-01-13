5fdb9542d51a00b89812270a6c96d83aac16126c
Али Джелялов <113125914+KReeDo47@users.noreply.github.com>
2023-01-13 12:49:39 +0300

    Комментирование mongo

--- GenshinWiki/createDB.js
+++ GenshinWiki/createDB.js
@@ -1,22 +1,21 @@
-var MongoClient = require('mongodb').MongoClient
-var data = require("./data.js").data
+//var MongoClient = require('mongodb').MongoClient
+//var data = require("./data.js").data
 
 
-const uri = "mongodb://127.0.0.1:27017/"
-const client = new MongoClient(uri)
-async function run() {
-try {
-await client.connect();
-var database = client.db("GenshinWiki");
-database.dropDatabase()
-database = client.db("GenshinWiki");
-const mystics = database.collection("mystics");
-const result = await mystics.insertMany(data);
-console.log(`${result.insertedCount} documents were inserted`);
-} finally {
-await client.close();
-}
-}
-run()
-
+//const uri = "mongodb://127.0.0.1:27017/"
+//const client = new MongoClient(uri)
+//async function run() {
+//try {
+//await client.connect();
+//var database = client.db("GenshinWiki");
+//database.dropDatabase()
+//database = client.db("GenshinWiki");
+//const mystics = database.collection("mystics");
+//const result = await mystics.insertMany(data);
+//console.log(`${result.insertedCount} documents were inserted`);
+//} finally {
+//await client.close();
+//}
+//}
+//run()
 
