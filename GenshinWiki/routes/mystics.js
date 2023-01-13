140e4a3b5be6c67806ebf0b6e1ccdf0be989a458
Али Джелялов <113125914+KReeDo47@users.noreply.github.com>
2023-01-13 12:36:24 +0300

    MySQL функционал пользователя

--- GenshinWiki/routes/mystics.js
+++ GenshinWiki/routes/mystics.js
@@ -1,21 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../mySQLConnect'); 
-//  var Mystic = require("../models/mystic").Mystic;
-//  var checkAuth = require("./../middleware/checkAuth.js")
-var async = require("async")
-
-
-
+var db = require('../mySQLConnect');
+var checkAuth = require("./../middleware/checkAuth.js")
+// var Mystic = require("../models/mystic").Mystic;
+// var async = require("async");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с mystics')
 });
 
-
 /* Страница мистиков на MySQL */
-router.get('/:nick' ,function(req, res, next) {
+router.get('/:nick', checkAuth, function(req, res, next) {
   db.query(`SELECT * FROM mystics WHERE mystics.nick = '${req.params.nick}'`, (err, mystics) => {
     if(err) {
       console.log(err);
@@ -32,18 +28,17 @@ router.get('/:nick' ,function(req, res, next) {
   })
 })
 
-
 /* Страница мистиков на mongodb*/
 // router.get('/:nick', checkAuth,function(req, res, next) {
-//  Mystic.findOne({nick:req.params.nick}, function(err,Mystic){ 
-//      if(err) return next(err)
-//      if(!Mystic) return next(new Error("Нет такого мистика в этой игре"))
-//      res.render('Mystic', {
-//          title: Mystic.title,
-//          picture: Mystic.avatar,
-//          desc: Mystic.desc
-//      })
+//     Mystic.findOne({nick:req.params.nick}, function(err, mystic){
+//     if(err) return next(err)
+//     if(!mystic) return next(new Error("Нет такого мистика в этой игре!"))
+//     res.render('mystic', {
+//       title: mystic.title,
+//       picture: mystic.avatar,
+//       desc: mystic.desc
+//     });
+//   })
 // })
-// })
-// .
+
 module.exports = router;
