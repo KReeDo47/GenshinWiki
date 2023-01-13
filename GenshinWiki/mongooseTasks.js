ecb511925bde1f2d5d78d29f58c1c8eee6bc6cc2
Али Джелялов <113125914+KReeDo47@users.noreply.github.com>
2023-01-13 12:52:07 +0300

    Комментирование mongo

--- GenshinWiki/mongooseTasks.js
+++ GenshinWiki/mongooseTasks.js
@@ -1,13 +1,13 @@
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/test_1')
-var Mystic = require("./models/mystic").Mystic
+//var mongoose = require('mongoose')
+//mongoose.connect('mongodb://127.0.0.1:27017/test_1')
+//var Mystic = require("./models/mystic").Mystic
 
-var mystic = new Mystic({
-    title: "Heydzo",
-    nick: "heydzo"
-})
+//var mystic = new Mystic({
+//    title: "Heydzo",
+//   nick: "heydzo"
+//})
 
-console.log(mystic)
-mystic.save(function(err, mystic, affected){
-console.log(mystic.title)    
-})
+//console.log(mystic)
+//mystic.save(function(err, mystic, affected){
+//console.log(mystic.title)    
+//})
